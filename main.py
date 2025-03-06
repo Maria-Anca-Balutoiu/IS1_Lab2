5218a889baae331d43b6ab634cf72b7c63b18868
Alex Vladut <alexvladut03@yahoo.com>
2025-03-06 14:07:47 +0200

    Add main.py to generate and display results every 5 seconds

---
+++ main.py
@@ -0,0 +1,8 @@
+import time
+
+while True:
+    # Generăm rezultatul (înlocuiește cu codul existent)
+    print("Rezultatul este regenerat.")
+    
+    # Așteaptă 5 secunde înainte de următoarea execuție
+    time.sleep(5)
